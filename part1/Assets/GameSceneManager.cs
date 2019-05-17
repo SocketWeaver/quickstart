5bc3ab754406182beebbfe2d70e2d7dfdc58826d
peter <peterzhou43@gmail.com>
2019-05-17 13:32:36 -0700

    first commit

---
+++ part1/Assets/GameSceneManager.cs
@@ -0,0 +1,18 @@
+﻿using SWNetwork;
+using UnityEngine;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public void OnSpawnerReady(bool alreadyFinishedSceneSetup)
+    {
+        if (!alreadyFinishedSceneSetup)
+        {
+            float positionX = Random.Range(-6.0f, 6.0f);
+            float positionY = Random.Range(-6.0f, 6.0f);
+
+            NetworkClient.Instance.LastSpawner.SpawnForPlayer(0, new Vector3(positionX, positionY, 0), Quaternion.identity);
+
+            NetworkClient.Instance.LastSpawner.PlayerFinishedSceneSetup();
+        }
+    }
+}
---
+++ part1/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using SWNetwork;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    NetworkID networkID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        networkID = GetComponent<NetworkID>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (networkID.IsMine)
+        {
+            float vertical = Input.GetAxis("Vertical");
+            float horizontal = Input.GetAxis("Horizontal");
+
+            float newX = transform.position.x + speed * horizontal * Time.deltaTime;
+            float newY = transform.position.y + speed * vertical * Time.deltaTime;
+
+            transform.position = new Vector3(newX, newY, 0);
+        }
+    }
+}
