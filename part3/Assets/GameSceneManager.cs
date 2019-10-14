4494b345c7f3956cb53c01095b4d55a22e2182df
PeterZ <socketweaver@gmail.com>
2019-10-14 08:06:44 -0700

    Updated to SWNetwork SDK 0.6.5

--- part3/Assets/GameSceneManager.cs
+++ part3/Assets/GameSceneManager.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 
 public class GameSceneManager : MonoBehaviour
 {
-    public void OnSpawnerReady(bool alreadyFinishedSceneSetup)
+    public void OnSpawnerReady(bool alreadyFinishedSceneSetup, SceneSpawner sceneSpawner)
     {
         if (!alreadyFinishedSceneSetup)
         {
             float positionX = Random.Range(-6.0f, 6.0f);
             float positionY = Random.Range(-6.0f, 6.0f);
 
-            NetworkClient.Instance.LastSpawner.SpawnForPlayer(0, new Vector3(positionX, positionY, 0), Quaternion.identity);
+            sceneSpawner.SpawnForPlayer(0, new Vector3(positionX, positionY, 0), Quaternion.identity);
 
-            NetworkClient.Instance.LastSpawner.PlayerFinishedSceneSetup();
+            sceneSpawner.PlayerFinishedSceneSetup();
         }
     }
 }
--- part3/Assets/SWNetwork/Scripts/Lobby.cs
+++ part3/Assets/SWNetwork/Scripts/Lobby.cs
@@ -104,7 +104,7 @@ public class Lobby : MonoBehaviour
     public void Play()
     {
         // Here we use the JoinOrCreateRoom method to get player into rooms quickly.
-        NetworkClient.Lobby.JoinOrCreateRoom(true, 2, 60, HandleJoinOrCreatedRoom);
+        NetworkClient.Lobby.JoinOrCreateRoom(true, 2, 20, HandleJoinOrCreatedRoom);
     }
 
     /* Lobby helper methods*/
